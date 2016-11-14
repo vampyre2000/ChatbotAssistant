5b8b1cfe793a617b4fb6b0728a4917eb63da7c3c
vampyre2000 <vampyre2000@gmail.com>
2016-11-14 22:18:19 +1100

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -7,3 +7,21 @@ import os
 import subprocess
 import time
 from datetime import datetime
+
+class Chatbot():
+	def __init__(self):
+		#Set this to True to enable speech.
+		self.SPEECHENABLED=False
+		##BOT VARIABLES
+		self.BOT={'FIRSTNAME':'Alice','LASTNAME':'Dunn','SEX':'female','AGE':'21','CITY':'Sydney','FAVOURITE_COLOUR':'Blue'}
+		
+  def SPEAK(self,words):
+		if self.SPEECHENABLED:
+			self.w="\'\"" + words + "\"\'"
+			print(words)
+			#os.system('espeak -v+f2 ' + self.w)
+			subprocess.run('espeak -v+f2 ' + self.w)
+			#call('espeak -v+f2 ' + self.w, shell=True)
+		else:
+			print(words)
+      
